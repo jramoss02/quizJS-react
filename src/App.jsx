1e80db3aaf1cd333cc59c73e67ced9ee7705bd74
José <02.jrsramos@gmail.com>
2023-04-13 14:38:10 -0300

    final-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import './App.css'
+// react, componentes, estáticos
+import { useContext, useEffect } from "react";
+import { QuizContext } from "./context/quiz";
+
+import Welcome from "./components/Welcome";
+import Question from "./components/Question";
+import GameOver from "./components/GameOver";
+
+import "./App.css";
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  useEffect(() => {
+    dispatch({type: "REORDER_QUESTIONS"})
+  }, []) 
 
   return (
     <div className="App">
-      <h1>Quiz app</h1>
+      <h1>Quiz de Programação</h1>
+      {quizState.gameStage == "Start" && <Welcome/>}
+      {quizState.gameStage == "Playing" && <Question/>}
+      {quizState.gameStage == "End" && <GameOver/>}
     </div>
     
   )
 }
 
-export default App
+export default App;
